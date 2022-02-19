3da688a6f4ce37a0bb627f5726a3a004e35931c3
Anita Yip <ceruleanox@gmail.com>
2022-02-19 18:29:17 -0500

    have cards for interactive Expense Tracker, list of income transactions and list of expense transactions

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { Grid } from '@material-ui/core';
+
+import Main from './components/Main/Main';
+import Details from './components/Details/Details';
+import useStyles from './styles';
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-    Hello!
+      <Grid className={classes.grid} container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh'}}>
+        <Grid item xs={12} sm={4}>
+          <Details title="Income"/>        
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />        
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expenses"/>        
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ expense-tracker/src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
+
+import useStyles from './styles';
+
+const Main = () => {
+    
+   const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="A dollar a day..."></CardHeader>
+            <CardContent>
+                <Typography align="center" variant="h5">Total Balance $100 </Typography>
+                <Typography align="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px'}}>
+                 {/* info card*/ }
+                 Try saying: Add income for $100 in category Salary for Monday 
+                </Typography>
+                <Divider />
+                {/* Form */}
+            </CardContent>
+            <CardContent className={classes.cardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                    {/* list */}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default Main
