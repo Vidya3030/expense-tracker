337700bb2223d704b438425ce1bcf59a73a3b3ce
Thi Le <ythi@this-air.wireless-1x.unc.edu>
2022-02-19 17:45:29 -0500

    basic 6 jars

---
+++ src/db.js
@@ -0,0 +1,34 @@
+const Jars = [
+    {
+        key: 1, 
+        category: "Necessity",
+        dollar: "$30"
+    },
+    {
+        key: 2, 
+        category: "Necessity",
+        dollar: "$30"
+    },
+    {
+        key: 3, 
+        category: "Necessity",
+        dollar: "$30"
+    },
+    {
+        key: 4, 
+        category: "Necessity",
+        dollar: "$30"
+    },
+    {
+        key: 5, 
+        category: "Necessity",
+        dollar: "$30"
+    },
+    {
+        key: 6, 
+        category: "Necessity",
+        dollar: "$30"
+    }
+]
+
+export default Jars
