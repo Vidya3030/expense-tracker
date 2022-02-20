173de34963668c060755bd65dfd6e08be10411ff
Thi Le <ythi@this-air.wireless-1x.unc.edu>
2022-02-19 23:13:49 -0500

    take input and calc

--- src/db.js
+++ src/db.js
@@ -2,32 +2,32 @@ const Jars = [
     {
         key: 1, 
         category: "Necessity",
-        dollar: "$30"
+        dollar: 50
     },
     {
         key: 2, 
         category: "Financial Freedom",
-        dollar: "$30"
+        dollar: 10
     },
     {
         key: 3, 
         category: "Long-term Savings",
-        dollar: "$30"
+        dollar: 10
     },
     {
         key: 4, 
         category: "Education",
-        dollar: "$30"
+        dollar: 10
     },
     {
         key: 5, 
         category: "Play",
-        dollar: "$30"
+        dollar: 10
     },
     {
         key: 6, 
         category: "Give",
-        dollar: "$30"
+        dollar: 10
     }
 ]
 
