e84238faa7ae72b1ab96b3505aecf2c604740440
Sneha Jaikumar <snehaj@email.unc.edu>
2022-02-20 03:25:55 -0500

    finish all features

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.css'
 import Jars from "./db"
 import SpeechTest from './components/SpeechTest'
 import React, { useState } from "react"
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import {useSpeechSynthesis} from 'react-speech-kit'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-function App() {
+const App = () => {
+
+  async function askIncome() {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = `What is your weekly income?`;
+    await window.speechSynthesis.speak(msg);
+  }
 
   const generateJars = (jar) => {
     return (
-      <SpeechTest key={jar.key} dollar={`$` + Math.round(income * (jar.dollar / 100)) / 100} category={jar.category} />
+      <SpeechTest key={jar.key} dollar={`$` + realIncome * (jar.dollar / 100)} category={jar.category} />
     )
   }
 
-  const [income, changeIncome] = useState("")
-  const [hasIncome, changeIncomeStatus] = useState(false)
+  const [realIncome, changeIncome] = useState("")
 
   const handleChangeIncome = (e) => {
     changeIncome(e.target.value)
@@ -38,31 +42,37 @@ function App() {
     }
   }
 
-  const commands = [
+  const commands=[
     {
       command: 'reset',
-      callback:({resetTranscript}) => resetTranscript(),
+      callback:({resetTranscript}) => resetTranscript()
     },
     {
-      command: 'what is amount in *',
+      command: 'how much do I have left in *',
       callback: (section) => {
         //create new var to store section amount
         for (let i = 0; i < Jars.length; i++) {
-          if (section == Jars[i].category) {
-            console.log('reached here')
+          if (section.toUpperCase() == (Jars[i].category).toUpperCase()) {
+            console.log((realIncome * (Jars[i].dollar / 100)))
+            var msg = new SpeechSynthesisUtterance();
+            msg.text = `${(realIncome * (Jars[i].dollar / 100))} dollars`;
+            window.speechSynthesis.speak(msg);
           }
         }
-
-        }
-        //read out loud the amount
-        
-        // document.body.style.background = color;
-      
-
+      }
     },
     {
-      command: 'stop',
-      callback:({stopListening}) => SpeechRecognition.stopListening,
+      command: 'change background color to *',
+      callback:(color) =>{
+        document.body.style.background = color;
+      }
+    },
+    {
+      command: 'my weekly income is *',
+      callback: (i) => {
+        console.log(i)
+        changeIncome(i)
+      }
     }
   ]
 
@@ -78,8 +88,34 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Keep them Jars green</h1>
+    <div class="App">
+      <button onClick={() => {
+        askIncome()
+        setTimeout(() => {
+          SpeechRecognition.startListening()
+        }, 2000)
+      }}>Income input</button>
+      <h1>Trackio</h1>
+      {realIncome}
+      <div className="jar-container">
+      {Jars.map(generateJars)}
+      </div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default App;
+
+
+
+
+//   return (
+//     <div className="App">
+      {/* <h1>Keep them Jars green</h1>
       
       <input 
       onChange={(e) => {
@@ -101,34 +137,45 @@ function App() {
       <div className="jar-container">
       {Jars.map(generateJars)}
       </div>
-      {income}
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-    </div>
-  );
-}
-
-///////////////////////////////////////
-  
-  // const Speech = () => {
-  //   const [value, setValue] = React.useState("");
-  //   const { speak } = useSpeechSynthesis();
-  //   return
-      
-
-  // }
-
-  
-
-
-
-// function App() {
-
-  
+      {income} */}
+//       {/* <p>Microphone: {listening ? 'on' : 'off'}</p> */}
+//       <button onClick={SpeechRecognition.startListening({continuous: true})}>Start</button>
+//       <button onClick={SpeechRecognition.stopListening}>Stop</button>
+//       <button onClick={resetTranscript}>Reset</button>
+//       <p>{transcript}</p>
+//     </div>
+//   )
 // }
 
-export default App;
-////////////////////////////////////
+// // const App = () =>{
+// //   const commands = [
+// //     {
+// //       command: 'reset',
+// //       callback:({resetTranscript}) => resetTranscript(),
+// //     }
+// //   ]
+
+// // };
+
+
+// ///////////////////////////////////////
+  
+//   // const Speech = () => {
+//   //   const [value, setValue] = React.useState("");
+//   //   const { speak } = useSpeechSynthesis();
+//   //   return
+      
+
+//   // }
+
+  
+
+
+
+// // function App() {
+
+  
+// // }
+
+// export default App
+// ////////////////////////////////////
